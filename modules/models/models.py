06486788c20e2dd3bda81b3931fd3ab20046aee9
uday214125 <uds214125@gmail.com>
2018-07-28 11:45:54 +0530

    Updated models.py

--- modules/models/models.py
+++ modules/models/models.py
@@ -1,17 +1,12 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_security import UserMixin, RoleMixin
-
-# db = SQLAlchemy()
+# Using mongoengine 
 
 # class User(db.Model, UserMixin):
     # email = db.Column(db.String(255), unique=True)
     # password = db.Column(db.String(255))
     # active = db.Column(db.Boolean())
-    # roles = db.relationship('Role', secondary=roles_users,
-    #                         backref=db.backref('users', lazy='dynamic'))
-
+    
     # def __init__(self, email, password, active, roles):
     #     self.email = email
     #     self.password = password
     #     self.active = active
-    #     self.roles = roles
+    
